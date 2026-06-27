cfb53263ec0e90bd49006fa465db49d41ca81b95
Noisy_Bomb <vladimirylanov98@gmail.com>
2026-06-27 17:43:08 +0300

    Created models for my future project in Core off app

---
+++ ExpenseTracker.Core/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpenseTracker.Core.Models;
+
+public class Category
+{
+    public Guid Id {get; set;}
+    public string Name { get; set; } = string.Empty;
+    public string Color {get; set;} =  string.Empty;
+    public Guid? UserId { get; set; }
+}
---
+++ ExpenseTracker.Core/Models/Expense.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpenseTracker.Core.Models;
+
+public class Expense
+{
+    public Guid Id {get; set;}
+    public decimal Amount {get; set;}
+    public string Description { get; set; } = string.Empty;
+    public DateTime Date {get; set;}
+    public Guid CategoryId {get; set;}
+    public Guid UserId {get; set;}
+}
